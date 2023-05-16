f13b52fb72c8c6400f14e70532a8e0525b2ce930
KungRaseri <countjoshington@gmail.com>
2023-05-15 23:18:11 -0700

    Enabled Cors, started working on world preview endpoint

--- Api/Controllers/WorldController.cs
+++ Api/Controllers/WorldController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -17,11 +18,21 @@ public class WorldController : ControllerBase
 
     [HttpPost]
     [Route("preview")]
-    public async Task<IActionResult> Preview()
+    public async Task<IActionResult> Preview([FromForm] WorldPreviewSettings worldPreviewSettings)
     {
-        var accounts = _context.Set<Account>();
+        var mapOptions = JsonSerializer.Deserialize<MapOptions>(worldPreviewSettings.MapOptions);
+        var biomes = JsonSerializer.Deserialize<Biome[]>(worldPreviewSettings.Biomes);
+        var elevationOptions = JsonSerializer.Deserialize<Options>(worldPreviewSettings.ElevationOptions);
+        var precipitationOptions = JsonSerializer.Deserialize<Options>(worldPreviewSettings.PrecipitationOptions);
+        var temperatureOptions = JsonSerializer.Deserialize<Options>(worldPreviewSettings.TemperatureOptions);
 
-        return Ok(accounts);
+        return Ok(new
+        {
+            world = new World { },
+            regions = new Region[] { },
+            tiles = new Tile[] { },
+            plots = new Plot[] { }
+        });
     }
 
     [HttpPost]
@@ -32,6 +43,34 @@ public class WorldController : ControllerBase
     }
 }
 
+public class WorldPreviewSettings
+{
+    public string MapOptions { get; set; }
+    public string Biomes { get; set; }
+    public string ElevationOptions { get; set; }
+    public string PrecipitationOptions { get; set; }
+    public string TemperatureOptions { get; set; }
+}
+
+public class MapOptions
+{
+    public string serverId { get; set; }
+    public string worldName { get; set; }
+    public int width { get; set; }
+    public int height { get; set; }
+    public long elevationSeed { get; set; }
+    public long precipitationSeed { get; set; }
+    public long temperatureSeed { get; set; }
+}
+
+public class Options
+{
+    public decimal amplitude;
+    public decimal frequency;
+    public int octaves;
+    public decimal persistence;
+    public int scale;
+}
 public class WorldPreviewResponse
 {
 
--- Api/Program.cs
+++ Api/Program.cs
@@ -9,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var corsPolicyName = "_corsPolicy-Browser-Game";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsPolicyName, policy =>
+    {
+        policy.WithOrigins("http://localhost:3000");
+    });
+});
+
 var dbConfiguration = builder.Configuration.GetSection("DatabaseSettings");
 
 builder.Services.Configure<DatabaseSettings>(dbConfiguration);
@@ -27,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(corsPolicyName);
 
 app.UseAuthorization();
 
